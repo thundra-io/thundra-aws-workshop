27493ddc1d665c9bc5dc6f49dc00546ac37164ab
meminb <mu.eminb_97@hotmail.com>
2022-02-02 16:31:03 +0300

    contentRetriever Funtion update

--- thundra-aws-workshop-demo-project/thundra-workshop-lambda-webcontent-retriever/src/common/constants.js
+++ thundra-aws-workshop-demo-project/thundra-workshop-lambda-webcontent-retriever/src/common/constants.js
@@ -1,10 +1,10 @@
-const AWS_DYNAMODB_TABLE_NAME = process.env.DYNAMODB_TABLE_NAME;
-const AWS_S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+const DYNAMODB_TABLE_NAME = process.env.DYNAMODB_TABLE_NAME;
+const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
 
 const AWS = require('aws-sdk');
 
 module.exports = {
     AWS,
-    AWS_DYNAMODB_TABLE_NAME,
-    AWS_S3_BUCKET_NAME
+    DYNAMODB_TABLE_NAME,
+    S3_BUCKET_NAME
 };
