980cd6d05fd7a5c98d4018c93908fb491af2d685
Emin Bilgiç <47768192+meminb@users.noreply.github.com>
2022-02-03 11:19:04 +0300

    Update constants.js

--- thundra-aws-workshop-demo-project/thundra-workshop-lambda-webcontent-retriever/src/common/constants.js
+++ thundra-aws-workshop-demo-project/thundra-workshop-lambda-webcontent-retriever/src/common/constants.js
@@ -1,11 +1,11 @@
-const DYNAMODB_TABLE_NAME = process.env.DYNAMODB_TABLE_NAME;
-const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+const AWS_DYNAMODB_TABLE_NAME = process.env.DYNAMODB_TABLE_NAME;
+const AWS_S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
 
 const AWS = require('aws-sdk');
 
 module.exports = {
     AWS,
-    DYNAMODB_TABLE_NAME,
-    S3_BUCKET_NAME
+    AWS_DYNAMODB_TABLE_NAME,
+    AWS_S3_BUCKET_NAME
 };
 
